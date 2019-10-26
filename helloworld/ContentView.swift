5c0c67efd4fc93f20277c7cf9bbb6e010ac3f243
halozxc <824927872@qq.com>
2019-10-26 22:18:02 +0800

    Initial Commit

---
+++ helloworld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  helloworld
+//
+//  Created by 奚锐的Mac on 2019/10/26.
+//  Copyright © 2019 奚锐的Mac. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
